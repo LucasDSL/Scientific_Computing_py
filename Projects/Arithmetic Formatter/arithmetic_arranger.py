5c81d1acca6c6f3f69e0d0c313a782835b9e1ee0
LucasDSL <lucasdslima2002@gmail.com>
2021-08-23 20:40:03 -0300

    arithmetic formatter complete and working properly according to test_modules.py

--- Projects/Arithmetic Formatter/arithmetic_arranger.py
+++ Projects/Arithmetic Formatter/arithmetic_arranger.py
@@ -1,4 +1,4 @@
-def arithmetic_arranger(list_sums, option_argument=True):
+def arithmetic_arranger(list_sums, option_argument=False):
     # Transforming all string on list of strings: 
     for i in range(0, len(list_sums)):
         list_sums[i] = list_sums[i].split(" ")    
@@ -17,8 +17,6 @@ def arithmetic_arranger(list_sums, option_argument=True):
         except: 
             return "Error: Numbers must only contain digits."
 
-    if not option_argument:
-        return ""
     string_return = ""
     # Formating the first line of the result. 
     for sum in list_sums:
@@ -27,7 +25,7 @@ def arithmetic_arranger(list_sums, option_argument=True):
         sz_n2 = len(sum[2]) # Size in digits of the second number.
         size_bigger_num = sz_n1 if sz_n1 > sz_n2 else sz_n2
         sum.append(size_bigger_num)
-        string_return += f"{sum[0]:>{size_bigger_num + 2}}" # Plus two spaces (1 for the
+        string_return += f"{sum[0]:>{sum[3] + 2}}" # Plus two spaces (1 for the
         # operation sign below and one for the blank space after it )
         if list_sums[-1] == sum:
             string_return += "\n"
@@ -46,25 +44,27 @@ def arithmetic_arranger(list_sums, option_argument=True):
     # Formating the line below the operation: 
     for sum in list_sums:
         string_return += "-" * (sum[3] + 2) # Plus two again (1 sign + 1 blank space)
-        if list_sums[-1] == sum:
+        if list_sums[-1] == sum and option_argument:
             string_return += "\n"
+        elif list_sums[-1] == sum and not option_argument:
+            string_return += ""
         else:
             string_return += "    "
 
-    # Getting the result from the sum of the two operands: 
-    for sum in list_sums:
-        current_sum = 0
-        if sum[1] == "+":
-            current_sum += int(sum[0]) + int(sum[2])
-        else: 
-            current_sum += int(sum[0]) - int(sum[2])
-        current_sum = str(current_sum)
-        size_current_sum = len(current_sum)
-        string_return += f"{current_sum:>{sum[3] + 2}}"
-        if list_sums[-1] == sum:
-            string_return += "\n"
-        else:
-            string_return += "    "
+    if option_argument:
+        # Getting the result from the sum of the two operands: 
+        for sum in list_sums:
+            current_sum = 0
+            if sum[1] == "+":
+                current_sum += int(sum[0]) + int(sum[2])
+            else: 
+                current_sum += int(sum[0]) - int(sum[2])
+            current_sum = str(current_sum)
+            string_return += f"{current_sum:>{sum[3] + 2}}"
+            if list_sums[-1] == sum:
+                string_return += ""
+            else:
+                string_return += "    "
 
     return string_return
 
--- Projects/Arithmetic Formatter/my_test.py
+++ Projects/Arithmetic Formatter/my_test.py
@@ -1,4 +1,3 @@
 from arithmetic_arranger import arithmetic_arranger as aa 
 
 print(aa(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]))
-print(aa(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]))
