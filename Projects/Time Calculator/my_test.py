9c3ea37e9dca1c8ecf3a45deea74a5fd771c4f79
LucasDSL <lucasdslima2002@gmail.com>
2021-08-25 10:08:04 -0300

    logic of the minutes and its convertion when the sum up to one hour or/and their sum go past a day done

---
+++ Projects/Time Calculator/my_test.py
@@ -0,0 +1,4 @@
+from time_calculator import add_timer as at
+
+at("11:11 PM", "2:55")
+
---
+++ Projects/Time Calculator/time_calculator.py
@@ -0,0 +1,29 @@
+def add_timer(start_time, duration, day_week=""):
+    week_days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"]
+    start_time = start_time.split(" ")
+    # Get the hours apart from the minutes and puts in the first position
+    start_time.insert(0, start_time[0].split(":"))
+    # Remove the format that has ":"
+    start_time.remove(start_time[1])
+    duration = duration.split(":")
+    total_minutes = int(duration[1]) + int(start_time[0][1])
+    print(total_minutes)
+    final_hour = 0
+    days_later = 0
+    final_minutes = 0
+    old_hour = start_time[0][0]
+    old_period = start_time[1]
+    ''' Calculating the minutes in two situations: 
+    first if the sum of the starting and duration numbers are bigger than 60,
+    whereas there will be one your more hour and where there are not more than
+    60 minutes.'''
+    if  total_minutes > 60:
+        final_hour += int(start_time[0][0]) + 1
+        minutes_left = total_minutes - 60
+        final_minutes += minutes_left
+    else: 
+        final_minutes += int(start_time[0][1]) + int(duration[1])
+    if old_hour == "11" and old_period == "PM" and final_hour == 12:
+        days_later += 1
+        old_period = "AM"
+    print(final_hour, final_minutes, days_later)
