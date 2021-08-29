2a0fc2a0c436e307fa43ddfb9b5e4be6ceaf69eb
LucasDSL <lucasdslima2002@gmail.com>
2021-08-28 23:32:39 -0300

    deleting the main function - going to restart the project soon

--- Projects/Time Calculator/my_test.py
+++
@@ -1,7 +0,0 @@
-from time_calculator import add_timer as at
-
-''' Tests for the implementation of the minutes 
-at("11:11 PM", "2:55")
-at("12:25 AM", "01:22")
-'''
-
--- Projects/Time Calculator/time_calculator.py
+++ Projects/Time Calculator/time_calculator.py
@@ -1,30 +1,5 @@
+from typing import final
+
+
 def add_timer(start_time, duration, day_week=""):
-    week_days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"]
-    start_time = start_time.split(" ")
-    # Get the hours apart from the minutes and puts in the first position
-    start_time.insert(0, start_time[0].split(":"))
-    # Remove the format that has ":"
-    start_time.remove(start_time[1])
-    # The same as above on duration
-    duration = duration.split(":")
-    total_minutes = int(duration[1]) + int(start_time[0][1])
-    final_hour = 0
-    days_later = 0
-    final_minutes = 0
-    # Get the beggining hour and period of time: AM or PM
-    old_hour = start_time[0][0]
-    old_period = start_time[1]
-    ''' Calculating the final minutes in two situations: 
-    first if the sum of the starting and duration numbers are bigger than 60,
-    whereas there will be one your more hour and where there are not more than
-    60 minutes.'''
-    if  total_minutes > 60:
-        final_hour += int(start_time[0][0]) + 1
-        minutes_left = total_minutes - 60
-        final_minutes += minutes_left
-    else: 
-        final_minutes += int(start_time[0][1]) + int(duration[1])
-    if old_hour == "11" and old_period == "PM" and final_hour == 12:
-        days_later += 1
-        old_period = "AM"
-    print(final_hour, final_minutes, days_later)
+    
